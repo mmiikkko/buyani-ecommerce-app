0e5ca57a9640f285d733bc5f9baa1108af2c55f9
kmartqt <martinsalamat848@gmail.com>
2025-11-05 18:04:57 +0800

    footer added

--- apps/web/src/app/(customer)/layout.tsx
+++ apps/web/src/app/(customer)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/nav/Navbar";
+import { Footer } from "@/app/(customer)/_components/footer";
 import { ReactNode } from "react";
 
 export default function CustomerLayout({ children }: { children: ReactNode }) {
@@ -6,6 +7,7 @@ export default function CustomerLayout({ children }: { children: ReactNode }) {
     <div>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 }
