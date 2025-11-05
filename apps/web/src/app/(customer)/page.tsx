1e4710e10351ae4c48c473f2cccbd53cbbb988ec
kmartqt <martinsalamat848@gmail.com>
2025-11-05 15:25:02 +0800

    Featured Vendors Added

--- apps/web/src/app/(customer)/page.tsx
+++ apps/web/src/app/(customer)/page.tsx
@@ -3,14 +3,16 @@
 import { ShopByCategorySection } from "./_components/shop-by-category"
 import { BackdropCarousel } from "./_components/backdrop-carousel";
 import { BestSellersSection } from "./_components/best-sellers";
+import { FeaturedVendorsSection } from "./_components/featured-vendors";
 
 export default function Home() {
 
   return (
     <main className="relative min-h-screen space-y-12">
       <BackdropCarousel/>
-      <ShopByCategorySection />
       <BestSellersSection />
+      <ShopByCategorySection />
+      <FeaturedVendorsSection />
     </main>
   );
 }
