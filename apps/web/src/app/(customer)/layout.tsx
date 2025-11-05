0539a6107074deb558ad677c321a936a3d87e994
User-Suuro <godwingalvez26@gmail.com>
2025-11-05 12:37:02 +0800

    minor change

--- apps/web/src/app/(customer)/layout.tsx
+++ apps/web/src/app/(customer)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/nav/Navbar";
+import Navbar from "@/components/nav/navbar";
 import { ReactNode } from "react";
 
 export default function CustomerLayout({ children }: { children: ReactNode }) {
