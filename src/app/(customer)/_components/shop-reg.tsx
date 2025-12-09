5ff9c617874e7b3efebc68c9e70466f4ce1ac0f6
mmiikkko <jaeceydellenn@gmail.com>
2025-12-10 06:58:13 +0800

    button

--- src/app/(customer)/_components/shop-reg.tsx
+++ src/app/(customer)/_components/shop-reg.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 import Logo from "@/assets/logo/Logo.png";
 import { LoadingButton } from "@/components/loading-button";
 import {
